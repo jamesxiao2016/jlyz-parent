4c42d9b62f2ab1ad00fa654fe8e2f376607eb179
肖伟 <james112496@163.com>
2018-05-18 11:05:04 +0800

    module change name

---
+++ dangjian-web/src/main/java/cn/dlbdata/dangjian/web/listener/DangjianStartupListener.java
@@ -0,0 +1,17 @@
+package cn.dlbdata.dangjian.web.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class DangjianStartupListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+
+	}
+
+}
