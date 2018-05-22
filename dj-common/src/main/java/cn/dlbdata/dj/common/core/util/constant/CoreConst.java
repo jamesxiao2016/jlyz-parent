0148dbde4b0f46dd9141d987caddcc990a100779
肖伟 <james112496@163.com>
2018-05-22 10:13:07 +0800

    consttant

--- dj-common/src/main/java/cn/dlbdata/dj/common/core/util/constant/CoreConst.java
+++ dj-common/src/main/java/cn/dlbdata/dj/common/core/util/constant/CoreConst.java
@@ -13,8 +13,7 @@ public class CoreConst {
 	public static final String SESSION_USER = "CURR_USER";
 	public static final String SESSION_ADMIN_USER = "ADMIN_USER";
 	public static final String SESSION_VAL_CODE = "VAL_CODE";
-	
-	
+
 	/**
 	 * 项目物理路径
 	 */
@@ -35,4 +34,40 @@ public class CoreConst {
 	 */
 	public static ApplicationContext springContext = null;
 
+	/**
+	 * 返回值状态
+	 * 
+	 * @author SW
+	 *
+	 */
+	public static enum ResultCode {
+		OK(200, "OK"), BadRequest(400, "Bad Request"), Unauthorized(401, "Unauthorized"), ParameterError(402,
+				"Parameter Error"), Forbidden(403, "Forbidden"), NotFound(404, "Not Found"), MethodNotAllowed(405,
+						"Method Not Allowed"), InternalServerError(500,
+								"500 Internal Server Error"), ServiceUnavailable(503, "Service Unavailable");
+		ResultCode(int code, String desc) {
+			this.code = code;
+			this.desc = desc;
+		}
+
+		int code;
+		String desc;
+
+		public int getCode() {
+			return code;
+		}
+
+		public void setCode(int code) {
+			this.code = code;
+		}
+
+		public String getDesc() {
+			return desc;
+		}
+
+		public void setDesc(String desc) {
+			this.desc = desc;
+		}
+
+	}
 }
--- dj-service/src/main/java/cn/dlbdata/dj/constant/DlbConstant.java
+++ dj-service/src/main/java/cn/dlbdata/dj/constant/DlbConstant.java
@@ -4,40 +4,4 @@ import cn.dlbdata.dj.common.core.util.constant.CoreConst;
 
 public class DlbConstant extends CoreConst {
 
-	/**
-	 * 返回值状态
-	 * 
-	 * @author SW
-	 *
-	 */
-	public static enum ResultCode {
-		OK(200, "OK"), BadRequest(400, "Bad Request"), Unauthorized(401, "Unauthorized"), ParameterError(402,
-				"Parameter Error"), Forbidden(403, "Forbidden"), NotFound(404, "Not Found"), MethodNotAllowed(405,
-						"Method Not Allowed"), InternalServerError(500,
-								"500 Internal Server Error"), ServiceUnavailable(503, "Service Unavailable");
-		ResultCode(int code, String desc) {
-			this.code = code;
-			this.desc = desc;
-		}
-
-		int code;
-		String desc;
-
-		public int getCode() {
-			return code;
-		}
-
-		public void setCode(int code) {
-			this.code = code;
-		}
-
-		public String getDesc() {
-			return desc;
-		}
-
-		public void setDesc(String desc) {
-			this.desc = desc;
-		}
-
-	}
 }
