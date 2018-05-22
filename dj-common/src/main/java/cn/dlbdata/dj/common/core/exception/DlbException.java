8c955066f24b21a368d1b07aa2ab93c0170dd495
肖伟 <james112496@163.com>
2018-05-22 12:29:08 +0800

    exception

---
+++ dj-common/src/main/java/cn/dlbdata/dj/common/core/exception/DlbException.java
@@ -0,0 +1,46 @@
+package cn.dlbdata.dj.common.core.exception;
+
+/**
+ * 异常处理类
+ * 
+ * @author xiaowei
+ *
+ */
+public class DlbException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	private int errorCode;
+	private String errorMsg;
+
+	public DlbException() {
+
+	}
+
+	public DlbException(String errorMsg) {
+		super(errorMsg);
+		this.errorMsg = errorMsg;
+	}
+
+	public DlbException(int errorCode, String errorMsg) {
+		super(errorMsg);
+		this.errorCode = errorCode;
+		this.errorMsg = errorMsg;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+}
