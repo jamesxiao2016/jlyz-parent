05cc011d627f72bb5dc57f1e8eaf7882b9f33357
肖伟 <james112496@163.com>
2018-05-30 17:36:42 +0800

    scoreType

---
+++ dj-db/src/main/java/cn/dlbdata/dj/db/vo/apply/ScoreTypeVo.java
@@ -0,0 +1,41 @@
+package cn.dlbdata.dj.db.vo.apply;
+
+public class ScoreTypeVo {
+	private Long id;
+	private String name;
+	private Float score;
+	private Float total;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getScore() {
+		return score;
+	}
+
+	public void setScore(Float score) {
+		this.score = score;
+	}
+
+	public Float getTotal() {
+		return total;
+	}
+
+	public void setTotal(Float total) {
+		this.total = total;
+	}
+
+}
