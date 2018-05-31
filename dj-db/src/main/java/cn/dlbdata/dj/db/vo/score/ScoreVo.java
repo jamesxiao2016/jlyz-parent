b749dcadbb32ac086711a19e27926b7d6ab9d1bb
zhouxuan2018 <18601773043@163.com>
2018-05-31 17:55:11 +0800

    解决积分明细返回值映射问题

--- dj-db/src/main/java/cn/dlbdata/dj/db/vo/score/ScoreVo.java
+++ dj-db/src/main/java/cn/dlbdata/dj/db/vo/score/ScoreVo.java
@@ -3,52 +3,68 @@ package cn.dlbdata.dj.db.vo.score;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Transient;
+
 public class ScoreVo implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	@Transient
 	private Long id;
 
 	/**
 	 * 二级类型ID
 	 */
+	@Transient
 	private Long djSubTypeId;
 
 	/**
 	 * 积分类型
 	 */
+	@Transient
 	private String typeName;
 
 	/**
 	 * 分数
 	 */
+	@Transient
 	private Float score;
 
 	/**
 	 * 用户ID
 	 */
+	@Transient
 	private Long userId;
 
 	/**
 	 * 加分时间
 	 */
+	@Transient
 	private Date addTime;
 
 	/**
 	 * 审批人ID
 	 */
+	@Transient
 	private Long approverId;
 
 	/**
 	 * 审批人姓名
 	 */
+	@Transient
 	private String approverName;
 	/**
 	 * 积分说明
 	 */
+	@Transient
 	private String scoreDesc;
+	
+	@Transient
+	private String recrodDesc;
+	
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -102,6 +118,12 @@ public class ScoreVo implements Serializable {
 	}
 	public void setScoreDesc(String scoreDesc) {
 		this.scoreDesc = scoreDesc;
+	}
+	public String getRecrodDesc() {
+		return recrodDesc;
+	}
+	public void setRecrodDesc(String recrodDesc) {
+		this.recrodDesc = recrodDesc;
 	}
 
 }
