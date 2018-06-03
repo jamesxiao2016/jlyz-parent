534ac77d618262322d0027cd9ca6e809ef2356e4
zhouxuan2018 <18601773043@163.com>
2018-06-03 13:40:39 +0800

    builPath解决ecpilse不编译

--- dj-serviceimpl/src/main/java/cn/dlbdata/dj/serviceimpl/ActiveServiceImpl.java
+++ dj-serviceimpl/src/main/java/cn/dlbdata/dj/serviceimpl/ActiveServiceImpl.java
@@ -2,8 +2,6 @@ package cn.dlbdata.dj.serviceimpl;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
