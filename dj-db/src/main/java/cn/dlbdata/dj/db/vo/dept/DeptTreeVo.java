1a54cada95f0373795a64b6253054b4fab6dfa75
ye <qiuxiyu90@163.com>
2018-06-12 18:27:34 +0800

    提交遗漏的Vo

---
+++ dj-db/src/main/java/cn/dlbdata/dj/db/vo/dept/DeptTreeVo.java
@@ -0,0 +1,42 @@
+package cn.dlbdata.dj.db.vo.dept;
+
+import java.util.List;
+
+public class DeptTreeVo {
+    private String name;
+    private Long deptId;
+    private Long parentId;
+    private List<DeptTreeVo> children;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public List<DeptTreeVo> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<DeptTreeVo> children) {
+        this.children = children;
+    }
+}
