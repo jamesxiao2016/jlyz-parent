cb096634cdb54cbb7ce4c8b042866877ed556598
肖伟 <james112496@163.com>
2018-06-28 17:33:32 +0800

    金领驿站展示接口

--- dj-serviceimpl/src/main/java/cn/dlbdata/dj/serviceimpl/JlyzServiceImpl.java
+++ dj-serviceimpl/src/main/java/cn/dlbdata/dj/serviceimpl/JlyzServiceImpl.java
@@ -1,7 +1,16 @@
 package cn.dlbdata.dj.serviceimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cn.dlbdata.dj.db.mapper.DjBuildingMapper;
+import cn.dlbdata.dj.db.mapper.DjDeptMapper;
+import cn.dlbdata.dj.db.mapper.DjSectionMapper;
+import cn.dlbdata.dj.db.mapper.TStatisticMapper;
+import cn.dlbdata.dj.db.pojo.DjSection;
 import cn.dlbdata.dj.db.vo.jlyz.BuildingVo;
 import cn.dlbdata.dj.db.vo.jlyz.MemberStatVo;
 import cn.dlbdata.dj.db.vo.jlyz.PartyBranchVo;
@@ -10,29 +19,62 @@ import cn.dlbdata.dj.service.IJlyzService;
 import cn.dlbdata.dj.serviceimpl.base.BaseServiceImpl;
 
 public class JlyzServiceImpl extends BaseServiceImpl implements IJlyzService {
+	@Autowired
+	private DjSectionMapper sectionMapper;
+	@Autowired
+	private DjBuildingMapper buildingMapper;
+	@Autowired
+	private DjDeptMapper deptMapper;
+	@Autowired
+	private TStatisticMapper statisticMapper;
 
 	@Override
 	public SectionVo querySection(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		SectionVo result = new SectionVo();
+		if (id == null) {
+			return result;
+		}
 
-	@Override
-	public BuildingVo queryBuilding(String buildingCode) {
-		// TODO Auto-generated method stub
-		return null;
+		// 查询片区信息
+		DjSection section = sectionMapper.selectByPrimaryKey(id);
+		if (section == null) {
+			return result;
+		}
+		// 查询片区负责人信息
+
+		// 查询楼宇列表
+
+		return result;
 	}
 
 	@Override
 	public List<PartyBranchVo> queryPartyBranches(String buildingCode) {
-		// TODO Auto-generated method stub
-		return null;
+		List<PartyBranchVo> result = new ArrayList();
+		if (StringUtils.isEmpty(buildingCode)) {
+			return null;
+		}
+		// 查询楼宇的党支部信息
+
+		return result;
+	}
+
+	@Override
+	public BuildingVo queryBuilding(String buildingCode) {
+		BuildingVo result = new BuildingVo();
+		if (StringUtils.isEmpty(buildingCode)) {
+			return result;
+		}
+
+		return result;
 	}
 
 	@Override
 	public MemberStatVo queryMemberStatistic(String buildingCode) {
-		// TODO Auto-generated method stub
-		return null;
+		MemberStatVo result = new MemberStatVo();
+		if (StringUtils.isEmpty(buildingCode)) {
+			return result;
+		}
+		return result;
 	}
 
 }
